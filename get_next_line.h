e5168c7ba1675f1faf2e2e66326d60bc1ca736a8
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2021-09-06 21:52:17 -0300

    gg

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rruiz-la <rruiz-la@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 18:44:54 by rruiz-la          #+#    #+#             */
+/*   Updated: 2021/09/06 16:52:10 by rruiz-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+void	arrange_backup(char **backup_buffer, int *i);
+char	*extract_line(char **buffer);
+int		read_file(int fd, char **buffer, char **buffer_backup);
+char	*get_line(int fd, char **buff, char **backup_buff);
+char	*get_next_line(int fd);
+char	*gnl_join(char **s1, char **s2);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rruiz-la <rruiz-la@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 18:44:50 by rruiz-la          #+#    #+#             */
+/*   Updated: 2021/09/06 21:08:27 by rruiz-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*gnl_join(char **s1, char **s2)
+{
+	char	*swap;
+
+	swap = *s1;
+	*s1 = ft_strjoin((const char *)swap, (const char *)*s2);
+	free(swap);
+	swap = NULL;
+	return (*s1);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 || !s2)
+	{
+		return (NULL);
+	}
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	s3 = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!s3)
+	{
+		return (NULL);
+	}
+	ft_strlcpy(s3, s1, len_s1 + 1);
+	ft_strlcpy(&s3[len_s1], s2, len_s2 + 1);
+	return (s3);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		dst[ft_strlen(dst)] = '\0';
+		return (ft_strlen(src));
+	}
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
